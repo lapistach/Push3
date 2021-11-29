5cf2eb5c4b779cf1404929c1135b4668991684f6
lapistach <91027035+lapistach@users.noreply.github.com>
2021-11-29 17:46:57 +0100

    Add files via upload

---
+++ 01test.py
@@ -0,0 +1,6 @@
+student_a = 'Ada Lovelace'
+student_b = 'Anita Borg'
+# inspired by https://www.fullstackacademy.com/blog/remarkable-women-programmers
+
+print('\n\nWe are ' + student_a + ' and ' + student_b + '.')
+print('\nAnyone can learn to code! The start might not be easy for everyone, but if you try, it will make click eventually! [And the teaching staff is there to catalyze the click, i.e. to help you ;-).]\n\n')
